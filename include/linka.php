c5e11122b63b60a053ba41984e64490ffa12d42f
renato barbosa de lima <renatobarbosadelimati@gmail.com>
2015-10-28 09:56:39 -0200

    commit inicial

---
+++ include/linka.php
@@ -0,0 +1,6 @@
+  <!--Página de links para os arquivos css e js--> 
+  <!-- .. JQuery (necessario para os plugins Javascript Bootstrap)..-->
+   <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+   <script src="http://code.jquery.com/jquery-latest.js"></script>
+   <script src="js/bootstrap.min.js"></script>
+		
---
+++ include/topo.php
@@ -0,0 +1,29 @@
+
+<div class="menu"  style=" background-color: #FF1493; font-size: 40px; ">
+    <?php
+     include "linka.php";
+    ?>
+	</br>
+	   <a href="#"> <p style=" margin-left: 390px; color:#ffffff; font-size: 40px; "> 
+	   <b>Renato a1320785</b>
+	   </p></a>
+	</br>
+	
+	  <div class="container" >  
+	  
+	        <ul class="nav nav-tabs">
+               <li class="active">
+               <a href="#"><b>Inicio</b></a>
+               </li>
+               <li><a href="#">Meio</a></li>
+               <li><a href="#">Fim</a></li>
+            </ul>
+      </div>
+       
+</div>
+	   
+
+
+
+
+	      
